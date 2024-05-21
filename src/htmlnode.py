8303b358a97ad379f1c0894c80707dfc44f07a6a
sol_fieldman <deanchimp@gmail.com>
2024-05-21 11:01:40 -0400

    added ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,19 +2,51 @@
 
 class HTMLNode:
     def __init__ (self,tag=None, value=None, children=None, props=None):
-        self.__tag = tag
-        self.__value = value
-        self.__children = children
-        self.__props = props
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
 
     def to_html(self):
-        raise Exception("Not yet implemented.")
+        raise NotImplementedError("Not implimented")
 
     def props_to_html(self):
         out = str()
-        for key in self.__props:
-            out += f' {key}="{self.__props[key]}"'
+        if not isinstance(self.props,dict):
+            return ''
+        for key in self.props:
+            out += f' {key}="{self.props[key]}"'
         return out
 
     def __repr__(self):
-        return f"HTMLnode({self.__tag}, {self.__value}, {self.__children}, {self.__props})"
+        return f"HTMLnode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__ (self,tag,value,props=None):
+        super().__init__(tag,value,None,props)
+        if not isinstance(self.value, str): raise ValueError("Must provide value.")
+
+    def to_html(self):
+        if self.tag == None:
+            return self.value
+        elif not isinstance(self.tag, str):
+            raise TypeError("Tag must be a string.")
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__ (self,tag,children,props=None):
+        super().__init__(tag,None,children,props)
+        if not isinstance(self.tag, str): raise ValueError("Must provide tag.")
+        if (not isinstance(self.children, list) or len(self.children) == 0):
+            raise ValueError("Invalid children list.")
+
+    def to_html(self):
+        out = [f"<{self.tag}>"]
+        for heir in self.children:
+            if isinstance(heir,HTMLNode): out+= heir.to_html()
+            elif isinstance(heir,str): out += heir
+            else: raise ValueError("Bastard Child!")
+        out += [f"</{self.tag}>"]
+        return ''.join(out)
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,8 +3,30 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
+
 
 class TestHTMLNode(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._leafnode = LeafNode('p','Sample Text',{"foo":"bar","fizz":"buzz"})
+        cls._leafnode_textonly = LeafNode(None, 'Sample Text', None)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_exceptions(self):
+        with self.assertRaises(ValueError):
+            print(LeafNode(None,None,None))
+
     def test_props_to_html(self):
-        node = HTMLNode(None,None,None,{"foo":"bar","fizz":"buzz"})
-        node = self.assertEqual(node.props_to_html(), ' foo="bar" fizz="buzz"')
+        self.assertEqual(self._leafnode.props_to_html(), ' foo="bar" fizz="buzz"')
+
+    def test_leaf_to_html(self):
+        self.assertEqual(
+            self._leafnode.to_html(),
+            '<p foo="bar" fizz="buzz">Sample Text</p>'
+        )
+        self.assertEqual(self._leafnode_textonly.to_html(), 'Sample Text')
